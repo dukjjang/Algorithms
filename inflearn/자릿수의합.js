d6d813b95fa676a154620ec3d953561263a3b202
dukjjang <jhd7292@naver.com>
2022-11-25 09:53:37 +0900

    자릿수의 합 리듀스로 풀이

--- inflearn/자릿수의합.js
+++ inflearn/자릿수의합.js
@@ -2,12 +2,11 @@ function solution(arr) {
   let answer = 0;
   let idx = 0;
 
-  arr = arr.map((x) => String(x));
   for (let i = 0; i < arr.length; i++) {
-    let cur = 0;
-    for (let j = 0; j < arr[i].length; j++) {
-      cur += Number(arr[i][j]);
-    }
+    let cur = arr[i]
+      .toString()
+      .split('')
+      .reduce((a, b) => a + Number(b), 0);
     if (cur > answer) {
       answer = cur;
       idx = i;
