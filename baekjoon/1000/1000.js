8aedcafdd7a0df58fc652c815a4093a2e8a8b41b
dukjjang <jhd7292@naver.com>
2022-12-21 12:27:01 +0900

    백준 1000 ~ 1003 풀이

---
+++ baekjoon/1000/1000.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "dev/stdin" : "input.txt";
+let input = fs.readFileSync(filePath).toString().trim();
+input = input.split(" ");
+let a = parseInt(input[0]);
+let b = parseInt(input[1]);
+
+function solution(num1, num2) {
+  console.log(num1 + num2);
+}
+solution(a, b);
